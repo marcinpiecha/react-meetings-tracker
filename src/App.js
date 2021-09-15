b1daa4e6d1903ac1e00f50dd5ecdd61e85e77efa
Marcin Piecha <marcinpiecha@Marcins-MacBook-Pro.local>
2021-09-15 22:24:45 +0100

    added toggle switches

--- src/App.js
+++ src/App.js
@@ -29,10 +29,16 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)
+      )
+  }
+
   return (
     <div className='container'>
       <Header title={'Meeting Tracker'}/>
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ( 'No Meetings for Today.' )}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ( 'No Meetings for Today.' )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -23,11 +23,11 @@ const tasks = [
 ]
 */
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task)=> (
-                <Task key={task.id} task={task} onDelete={onDelete} />
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )
